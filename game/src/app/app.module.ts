648a6e0e59cb68e31cdccf24326e7a7ff91c7f44
Kevin Haley <khaley319@gmail.com>
2019-01-28 16:18:16 -0800

    first

--- game/src/app/app.module.ts
+++ game/src/app/app.module.ts
@@ -9,6 +9,19 @@ import { MainComponent } from './main/main.component';
 import { HouseComponent } from './house/house.component';
 import { CastleComponent } from './castle/castle.component';
 import { SpaceComponent } from './space/space.component';
+import { FrontRoomComponent } from './house/front-room/front-room.component';
+import { GarageComponent } from './house/garage/garage.component';
+import { KitchenComponent } from './house/kitchen/kitchen.component';
+import { LibraryComponent } from './house/library/library.component';
+import { BathroomComponent } from './house/bathroom/bathroom.component';
+import { BasementComponent } from './house/basement/basement.component';
+import { ParlorComponent } from './house/parlor/parlor.component';
+import { MasterBedroomComponent } from './house/master-bedroom/master-bedroom.component';
+import { MyBedroomComponent } from './house/my-bedroom/my-bedroom.component';
+import { UpstairsLandingComponent } from './house/upstairs-landing/upstairs-landing.component';
+import { AtticComponent } from './house/attic/attic.component';
+import { BackyardComponent } from './house/backyard/backyard.component';
+import { ShedComponent } from './house/shed/shed.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +29,20 @@ import { SpaceComponent } from './space/space.component';
     MainComponent,
     HouseComponent,
     CastleComponent,
-    SpaceComponent
+    SpaceComponent,
+    FrontRoomComponent,
+    GarageComponent,
+    KitchenComponent,
+    LibraryComponent,
+    BathroomComponent,
+    BasementComponent,
+    ParlorComponent,
+    MasterBedroomComponent,
+    MyBedroomComponent,
+    UpstairsLandingComponent,
+    AtticComponent,
+    BackyardComponent,
+    ShedComponent
   ],
   imports: [
     BrowserModule,
