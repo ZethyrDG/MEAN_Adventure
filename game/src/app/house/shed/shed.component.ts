3dbba66a02b27770ccc0240674068651586ef934
Kevin Haley <khaley319@gmail.com>
2019-01-31 13:38:49 -0800

    house done!

--- game/src/app/house/shed/shed.component.ts
+++ game/src/app/house/shed/shed.component.ts
@@ -10,19 +10,22 @@ export class ShedComponent implements OnInit {
   description: String;
   switch: Boolean = true;
   switch1: Boolean = false;
+  imgSwap: Number = 0;
   constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
     if(this._httpService.user.progress.savedAlien == false || this._httpService.user.progress.savedthewizard == false){
-      this.description = "You step in the small dusty shed. There are a lot of gardening tools, including a long stick with a hook on the end. You see a word etched into the wall, 'Alohamora'"
+      this.description = "You step in the small dusty shed. There are a lot of gardening tools, including a long rope with a hook on the end. You see a word etched into the wall, 'Alohamora'"
       this._httpService.user.progress.shed = true;
     }
     if(this._httpService.user.inventory.hook == true){
       this.switch1 = true;
+      this.imgSwap = 1;
     }
     if(this._httpService.user.progress.savedAlien == true && this._httpService.user.progress.savedthewizard == true){
       this.description = "One of the large shelves inside the shed has fallen, revealing a small trapdoor! Moving the fallen junk around the trapdoor, you open it. There's a very tight staircase leading down..."
       this.switch = false;
+      this.imgSwap = 2;
     }
   }
   param(num){
@@ -30,6 +33,7 @@ export class ShedComponent implements OnInit {
       this.description="You grab the hook and think this might be useful later."
       this._httpService.user.inventory.hook = true;
       this.switch1 = true;
+      this.imgSwap = 1;
     }
   }
 }
